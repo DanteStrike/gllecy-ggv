c8b7cefa75a394e4a06d914b611bfa5da92f4dba
Gevork Gevorkyan <bmstu.gg@gmail.com>
2018-08-14 12:59:26 +0300

    ADD slider behavior

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,6 @@
 
-const CONTAINER_BACKGROUNDS_LOC = `../img/ice-background--`;
-const INDEX_BACKGROUND_COLOR = `index--bg-`;
+const INDEX_BACKGROUND_COLOR_PREFIX = `index--bg`;
+const CONTAINER_BACKGROUND_PREFIX = `index__container--bg`;
 
 let index = document.querySelector(`.index`);
 let container = index.querySelector(`.container`);
@@ -17,19 +17,22 @@ let onSliderControlClick = function (evt) {
   let broker = evt.currentTarget;
 
   let activeButton = broker.querySelector(`.slider__control-btn--active`);
+  let activeId = activeButton.dataset.sliderId;
   let activeSlide = slides.querySelector(`.slider__item--active`);
 
   let target = evt.target;
-  let targetSliderId = target.dataset.sliderId;
-  let targetSlide = slides.querySelector(`[data-slider-id="${targetSliderId}"]`);
+  let targetId = target.dataset.sliderId;
+  let targetSlide = slides.querySelector(`[data-slider-id="${targetId}"]`);
 
   activeButton.classList.toggle(`slider__control-btn--active`);
   activeSlide.classList.toggle(`slider__item--active`);
+  index.classList.remove(`${INDEX_BACKGROUND_COLOR_PREFIX}-${activeId}`);
+  container.classList.remove(`${CONTAINER_BACKGROUND_PREFIX}-${activeId}`);
 
   target.classList.toggle(`slider__control-btn--active`);
   targetSlide.classList.toggle(`slider__item--active`);
-
-  index.classList.remove(``);
+  index.classList.add(`${INDEX_BACKGROUND_COLOR_PREFIX}-${targetId}`);
+  container.classList.add(`${CONTAINER_BACKGROUND_PREFIX}-${targetId}`);
 }
 
-sliderControl.addEventListener(`click`, onSliderControlClick)
+sliderControl.addEventListener(`click`, onSliderControlClick);
